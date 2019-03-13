b065ffb73dc7b73532c6225a540d85e288b448f4
DatOneRam <ramsey.alsheikh@gmail.com>
2019-03-13 17:25:48 -0400

    lmr i h

--- Test.java
+++ Test.java
@@ -3,6 +3,20 @@ public class Test
     public static void main(String args[])
     {
         Board b = new Board();
-        NimBot bob = new NimBot(b);
+        NimBot strat = new NimBot(b);
+        NimBot noStrat = new NimBot(b);
+        int cnt, stratWins = 0, noStratWins = 0;
+
+        for (cnt = 1; cnt <= 10000; cnt++)
+        {
+            do
+            {
+                strat.makeStrategicMove();
+                if (b.isFinished())
+                    break;
+                noStrat.makeRandomMove();
+            }
+            while (!b.isFinished());
+        }
     }
 }
