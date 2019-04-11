b8d5af5462493d713b533f1233a024ff0ceae40b
DatOneRam <ramsey.alsheikh@gmail.com>
2019-04-10 21:14:16 -0400

    added comment

--- TestStrat.java
+++ TestStrat.java
@@ -4,6 +4,7 @@ public class TestStrat
 {
     public static void main(String[] args)
     {
+        //note, will hang if match finishes early. just stop the code running and continue with your life
         Board b = new Board();
         NimBot bot = new NimBot(b);
         System.out.println(Arrays.toString(b.getLines()));
